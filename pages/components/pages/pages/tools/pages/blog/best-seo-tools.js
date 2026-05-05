281a82ac8687198efcbdd35f09fd634a4575064b
RISHABH <rboy29946@gmail.com>
2026-05-05 22:22:48 +0530

    Create best-seo-tools.js

---
+++ pages/components/pages/pages/tools/pages/blog/best-seo-tools.js
@@ -0,0 +1,18 @@
+import Layout from "../../components/Layout";
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Best Free SEO Tools in 2026</h1>
+
+      <p>
+        Discover the best free SEO tools to improve your website ranking.
+      </p>
+
+      <ul>
+        <li><a href="/tools/word-counter">Word Counter</a></li>
+        <li><a href="/tools/meta-generator">Meta Generator</a></li>
+      </ul>
+    </Layout>
+  );
+}
