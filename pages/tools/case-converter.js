ca0b42b49f8bc4bcf912695d80d8da266ef72751
RISHABH <rboy29946@gmail.com>
2026-05-06 10:17:54 +0530

    Update case-converter.js

--- pages/tools/case-converter.js
+++ pages/tools/case-converter.js
@@ -1,1 +1,24 @@
+import { useState } from "react";
+
+export default function CaseConverter() {
+  const [text, setText] = useState("");
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Case Converter</h1>
+
+      <textarea
+        rows="10"
+        style={{width:"100%"}}
+        onChange={(e)=>setText(e.target.value)}
+      />
+
+      <h3>UPPERCASE</h3>
+      <p>{text.toUpperCase()}</p>
+
+      <h3>lowercase</h3>
+      <p>{text.toLowerCase()}</p>
+    </div>
+  );
+}
 
