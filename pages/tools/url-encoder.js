025126c4c804de1874ba1431f8b1115e56bc346b
RISHABH <rboy29946@gmail.com>
2026-05-06 10:30:35 +0530

    Update url-encoder.js

--- pages/tools/url-encoder.js
+++ pages/tools/url-encoder.js
@@ -1,1 +1,19 @@
+import { useState } from "react";
 
+export default function UrlEncoder() {
+  const [text,setText] = useState("");
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>URL Encoder</h1>
+
+      <textarea
+        rows="8"
+        style={{width:"100%"}}
+        onChange={(e)=>setText(e.target.value)}
+      />
+
+      <pre>{encodeURIComponent(text)}</pre>
+    </div>
+  );
+}
