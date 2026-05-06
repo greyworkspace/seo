8cce95e268e030e68bea6ab87a522b1c2f4fc9a8
RISHABH <rboy29946@gmail.com>
2026-05-06 10:26:31 +0530

    Update uuid-generator.js

--- pages/tools/uuid-generator.js
+++ pages/tools/uuid-generator.js
@@ -1,1 +1,21 @@
+import { useState } from "react";
 
+export default function UUIDGenerator() {
+  const [uuid, setUuid] = useState("");
+
+  const generateUUID = () => {
+    setUuid(crypto.randomUUID());
+  };
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>UUID Generator</h1>
+
+      <button onClick={generateUUID}>
+        Generate UUID
+      </button>
+
+      <p>{uuid}</p>
+    </div>
+  );
+}
