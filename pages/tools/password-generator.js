ba6ea8e35abbce2a49fa4fd3459ebb6415155194
RISHABH <rboy29946@gmail.com>
2026-05-06 10:27:25 +0530

    Update password-generator.js

--- pages/tools/password-generator.js
+++ pages/tools/password-generator.js
@@ -1,1 +1,21 @@
+import { useState } from "react";
 
+export default function PasswordGenerator() {
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+    setPassword(Math.random().toString(36).slice(-12));
+  };
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Password Generator</h1>
+
+      <button onClick={generatePassword}>
+        Generate Password
+      </button>
+
+      <p>{password}</p>
+    </div>
+  );
+}
