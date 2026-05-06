c0628198ea7746348552f44917879cda69e975b9
RISHABH <rboy29946@gmail.com>
2026-05-06 10:08:02 +0530

    Update seo-tips.js

--- pages/blog/seo-tips.js
+++ pages/blog/seo-tips.js
@@ -1,1 +1,17 @@
+export default function SeoTips() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>SEO Tips for Beginners</h1>
 
+      <p>
+        Learn simple SEO tips to rank your website higher on Google search.
+      </p>
+
+      <ol>
+        <li>Use proper keywords</li>
+        <li>Write quality content</li>
+        <li>Optimize page speed</li>
+      </ol>
+    </div>
+  );
+}
