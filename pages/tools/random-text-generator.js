8cd14a9f2cb38159fdc658481937385be364e6a0
RISHABH <rboy29946@gmail.com>
2026-05-06 10:31:25 +0530

    Update random-text-generator.js

--- pages/tools/random-text-generator.js
+++ pages/tools/random-text-generator.js
@@ -1,1 +1,21 @@
+import { useState } from "react";
 
+export default function RandomTextGenerator() {
+  const [text,setText] = useState("");
+
+  const generateText = () => {
+    setText("Lorem ipsum dolor sit amet consectetur adipiscing elit.");
+  };
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Random Text Generator</h1>
+
+      <button onClick={generateText}>
+        Generate Text
+      </button>
+
+      <p>{text}</p>
+    </div>
+  );
+}
