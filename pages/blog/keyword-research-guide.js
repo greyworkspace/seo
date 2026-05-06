e7f3a9fd57284db91276483ef9a26591f8ae53bd
RISHABH <rboy29946@gmail.com>
2026-05-06 10:16:27 +0530

    Update keyword-research-guide.js

--- pages/blog/keyword-research-guide.js
+++ pages/blog/keyword-research-guide.js
@@ -1,1 +1,17 @@
+export default function KeywordResearchGuide() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Keyword Research Guide</h1>
 
+      <p>
+        Keyword research helps you find what users search on Google.
+      </p>
+
+      <ul>
+        <li>Use low competition keywords</li>
+        <li>Target long-tail keywords</li>
+        <li>Analyze search volume</li>
+      </ul>
+    </div>
+  );
+}
