4a4efc5ebf11d3bd1eb7238297ed631aec998b41
RISHABH <rboy29946@gmail.com>
2026-05-06 10:29:42 +0530

    Update base64-encoder.js

--- pages/tools/base64-encoder.js
+++ pages/tools/base64-encoder.js
@@ -1,1 +1,19 @@
+import { useState } from "react";
 
+export default function Base64Encoder() {
+  const [text,setText] = useState("");
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Base64 Encoder</h1>
+
+      <textarea
+        rows="8"
+        style={{width:"100%"}}
+        onChange={(e)=>setText(e.target.value)}
+      />
+
+      <pre>{btoa(text)}</pre>
+    </div>
+  );
+}
