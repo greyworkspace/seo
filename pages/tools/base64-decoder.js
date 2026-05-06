9a884ea87cd1b0f187c95f6421768e2a8c76c40e
RISHABH <rboy29946@gmail.com>
2026-05-06 10:30:08 +0530

    Update base64-decoder.js

--- pages/tools/base64-decoder.js
+++ pages/tools/base64-decoder.js
@@ -1,1 +1,27 @@
+import { useState } from "react";
 
+export default function Base64Decoder() {
+  const [text,setText] = useState("");
+
+  let decoded = "";
+
+  try {
+    decoded = atob(text);
+  } catch {
+    decoded = "Invalid Base64";
+  }
+
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Base64 Decoder</h1>
+
+      <textarea
+        rows="8"
+        style={{width:"100%"}}
+        onChange={(e)=>setText(e.target.value)}
+      />
+
+      <pre>{decoded}</pre>
+    </div>
+  );
+}
